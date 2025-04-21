344a991baf5e20c8870650383281ade55220a7e3
Miles <contact@smiles.dev>
2025-04-22 00:08:26 +0100

    add edit button to web ui

--- static/index.js
+++ static/index.js
@@ -16,7 +16,7 @@ function populateLights(data) {
 
         const existingLightCard = document.getElementById(ip);
         if (existingLightCard) {
-            existingLightCard.querySelector('#label').innerText = light.label;
+            existingLightCard.querySelector('#label').innerHTML = `${light.label} <svg class="edit-icon" onclick="editLabel('${ip}')" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#000"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h357l-80 80H200v560h560v-278l80-80v358q0 33-23.5 56.5T760-120H200Zm280-360ZM360-360v-170l367-367q12-12 27-18t30-6q16 0 30.5 6t26.5 18l56 57q11 12 17 26.5t6 29.5q0 15-5.5 29.5T897-728L530-360H360Zm481-424-56-56 56 56ZM440-440h56l232-232-28-28-29-28-231 231v57Zm260-260-29-28 29 28 28 28-28-28Z"/></svg>`;
             existingLightCard.querySelector('#power').innerHTML = `
                 <button class="power-button ${isOn ? 'on' : 'off'}" onclick="togglePower('${ip}')">
                     <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
@@ -40,7 +40,7 @@ function populateLights(data) {
         lightCard.classList.add('light-card');
 
         lightCard.innerHTML = `
-            <h2 id="label">${light.label}</h2>
+            <h2 id="label">${light.label} <svg class="edit-icon" onclick="editLabel('${ip}')" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#000"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h357l-80 80H200v560h560v-278l80-80v358q0 33-23.5 56.5T760-120H200Zm280-360ZM360-360v-170l367-367q12-12 27-18t30-6q16 0 30.5 6t26.5 18l56 57q11 12 17 26.5t6 29.5q0 15-5.5 29.5T897-728L530-360H360Zm481-424-56-56 56 56ZM440-440h56l232-232-28-28-29-28-231 231v57Zm260-260-29-28 29 28 28 28-28-28Z"/></svg></h2>
             <div class="controls">
                 <div id="power">
                     <button class="power-button ${isOn ? 'on' : 'off'}" onclick="togglePower('${ip}')">
@@ -114,6 +114,37 @@ async function refreshLightData() {
     } catch (error) {
         console.error('Error fetching light data:', error);
     }
+}
+
+function editLabel(ip) {
+    const lightCard = document.getElementById(ip);
+    const labelElement = lightCard.querySelector('#label');
+    const currentLabel = labelElement.innerText.replace(' Edit', '');
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.value = currentLabel;
+    input.classList.add('edit-label-input');
+    labelElement.replaceWith(input);
+    input.focus();
+    input.addEventListener('keypress', function(event) {
+        if (event.key === 'Enter') {
+            const newLabel = input.value;
+            fetch('/api/setName', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ ip: ip, name: newLabel })
+            })
+            .then(() => {
+                const newLabelEl = document.createElement('h2');
+                newLabelEl.id = 'label';
+                newLabelEl.innerHTML = `${newLabel} <svg class="edit-icon" onclick="editLabel('${ip}')" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#000"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h357l-80 80H200v560h560v-278l80-80v358q0 33-23.5 56.5T760-120H200Zm280-360ZM360-360v-170l367-367q12-12 27-18t30-6q16 0 30.5 6t26.5 18l56 57q11 12 17 26.5t6 29.5q0 15-5.5 29.5T897-728L530-360H360Zm481-424-56-56 56 56ZM440-440h56l232-232-28-28-29-28-231 231v57Zm260-260-29-28 29 28 28 28-28-28Z"/></svg>`;
+                input.replaceWith(newLabelEl);
+            })
+            .catch(error => console.error('Error:', error));
+        }
+    });
 }
 
 setInterval(refreshLightData, 500);
